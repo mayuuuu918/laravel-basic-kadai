ccbae3847980fd81ac12f0cd9301ce9d35329e22
mayuuuu918 <mayu.mt52@gmail.com>
2024-07-27 07:37:37 +0900

    課題提出

--- resources/views/posts/index.blade.php
+++ resources/views/posts/index.blade.php
@@ -9,6 +9,18 @@
 
 <body>
     <h1>投稿一覧</h1>
+    <table>
+        <tr>
+            <th>タイトル</th>
+            <th>本文</th>
+        </tr>
+        @foreach($posts as $post)
+            <tr>
+                <td><h5>{{ $post->title }}</h5></td>
+                <td><h5>{{ $post->content }}</h5></td>
+            </tr>
+        @endforeach
+    </table>
 </body>
 
 </html>
